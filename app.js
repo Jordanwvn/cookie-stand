ae1c8e0e33d2eb529b98a73be9bef014b3973616
Jordan Van Ness <jordanwvn@gmail.com>
2017-10-25 15:58:17 -0700

    now populates on user form input; removed footer

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var storeHours = ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM']; // business hours
+var storeArray = []; // array to hold all store objects
+var form = document.getElementById('store-form'); //
 
-//set consturctor for creating each store:
+//consturctor for creating each store:
 var Store = function(name, minCustomers, maxCustomers, avgCookies) {
   this.name = name; // location of store
   this.min = minCustomers; // minimum customers per day
@@ -19,23 +21,14 @@ Store.prototype.rdmCustomers = function() {
 
 // method that will generate hourly and daily sales, while updating the array and integer defined in the constructor
 Store.prototype.render = function() {
+  this.salesData.push(this.name); // pushes the name to the leftmost cell
 
-  // starts by making the first item in the array the name of the store
-  this.salesData.push(this.name);
-
-  for (var i = 6; i <= 20; i++) {
-    // generates the sales for the hour based on averages
-    var hourlyGeneratedSales = Math.floor(this.rdmCustomers() * this.avg);
-
-    // adds the generated hourly sales to the running total of the daily sales
-    this.dailySales += hourlyGeneratedSales;
-
-    // inputs the projected sales for this hour into the array of sales
-    this.salesData.push(hourlyGeneratedSales);
-
-    // if this is the last iteration, adds the daily sales to the array
-    if(i === 20){
-      this.salesData.push(this.dailySales);
+  for (var i = 0; i < storeHours.length; i++) { // for every hour of sales
+    var hourlyGeneratedSales = Math.floor(this.rdmCustomers() * this.avg); // generate hourly sales
+    this.salesData.push(hourlyGeneratedSales); // add hourly sales to the array of sales
+    this.dailySales += hourlyGeneratedSales; // add hourly sales to daily sales
+    if(i === storeHours.length - 1){ // if this is the last iteration
+      this.salesData.push(this.dailySales); // add total sales to the array of sales
     } // end if
   } // end for
 
@@ -57,7 +50,6 @@ Store.prototype.render = function() {
   table.appendChild(tableRow);
 
 } // end method
-
 
 var makeHeader = function() {
   var header = document.getElementById('table-header');
@@ -84,41 +76,63 @@ var makeHeader = function() {
 } // end makeHeader function
 
 
-var makeFooter = function(stores) {
-  var footer = document.getElementById('table-footer');
-  var footerRow = document.createElement('tr');
-  var footerData;
-
-  footerData = document.createElement('th');
-  footerData.innerHTML = 'Totals';
-  footer.appendChild(footerData);
-
-  for (var l = 1; l < storeHours.length + 2; l++) {
-
-    footerData = document.createElement('td');
-    var hourlyTotal = 0;
-
-    for (var m = 0; m < storeArray.length; m++) {
-      hourlyTotal += storeArray[m].salesData[l];
-    } // end m for
-
-    footerData.innerHTML = hourlyTotal;
-    footer.appendChild(footerData);
-  } // end l for
-  footer.appendChild(footerRow);
-} // end makeFooter function
+// var makeFooter = function(stores) {
+//   var footer = document.getElementById('table-footer');
+//   var table = document.getElementById('table');
+//   var footerRow = document.createElement('tr');
+//   var footerData;
+//
+//   footerData = document.createElement('th');
+//   footerData.innerHTML = 'Totals';
+//   footer.appendChild(footerData);
+//
+//   for (var l = 1; l < salesData.length + 2; l++) {
+//
+//     footerData = document.createElement('td');
+//     var hourlyTotal = 0;
+//
+//     for (var m = 0; m < storeArray.length; m++) {
+//       hourlyTotal += storeArray[m].salesData[l];
+//     } // end m for
+//
+//     footerData.innerHTML = hourlyTotal;
+//     footer.appendChild(footerData);
+//   } // end l for
+//   footer.appendChild(footerRow);
+// } // end makeFooter function
 
 // combines the table functions to one print command
-var printTable = function() {
-  makeHeader();
-  for (var n = 0; n < storeArray.length; n++) {
-    storeArray[n].render();
-  };
-  makeFooter(storeArray);
+// var printTable = function() {
+//   makeHeader();
+//   for (var n = 0; n < storeArray.length; n++) {
+//     storeArray[n].render();
+//   };
+//   makeFooter(storeArray);
+// }
+
+
+function formTable(event) {
+  event.preventDefault();
+
+  //makes a header for only the first value entered
+  if (storeArray.length === 0){
+    makeHeader();
+  }
+
+  var name = event.target.store.value;
+  var minCustomers = event.target.min.value;
+  var maxCustomers = event.target.max.value;
+  var avgCookieSales = event.target.avg.value;
+
+  storeArray.push(new Store(name, minCustomers, maxCustomers, avgCookieSales));
+  // printTable();
+  storeArray[storeArray.length-1].render();
+  // makeFooter();
+  form.reset();
 }
 
 // creating the stores with their data supplied by Pat
-var storeArray = [new Store('1st and Pike', 23, 65, 6.3), new Store('SeaTac Airport', 3, 23, 1.2), new Store('Seattle Center', 11, 38, 3.7), new Store('Capital Hill', 10, 38, 2.3), new Store('Alki', 2, 16, 4.6)];
+//var storeArray = [new Store('1st and Pike', 23, 65, 6.3), new Store('SeaTac Airport', 3, 23, 1.2), new Store('Seattle Center', 11, 38, 3.7), new Store('Capital Hill', 10, 38, 2.3), new Store('Alki', 2, 16, 4.6)];
 
 // calling the print function
-printTable();
+form.addEventListener('submit', formTable);
